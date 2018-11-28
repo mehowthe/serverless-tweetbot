6c5c7d781829c78fa7d1e7cb7575134538e7b2d6
mehowthe <drkr24@gmail.com>
2018-11-28 20:47:19 +0000

    Refactoring

--- messageCreator.js
+++ messageCreator.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const EMOJIS = require('./emojis');
-const TOO_GENERIC_LABELS = ['animal', 'mammal'];
+const TOO_GENERIC_LABELS = ['animal', 'mammal', 'pet'];
 
 class MessageCreator {
     
@@ -12,26 +12,27 @@ class MessageCreator {
           
         if (imageLabels.length < 2) {
             console.log('Not enough labels');
-            return reject('Not enough labels');
+            return resolve('I don\'t know... It\'s too hard for me 😞😞')
         }
-        
-        const names = imageLabels.map(label => label.Name);
+
+        const names = imageLabels.map(label => label.Name)
+            .map(label => label.toLowerCase());
         
         const isAnimal = names
-            .filter(label => label === 'Animal')
+            .filter(label => label === 'animal')
             .length == 1;
 
         const animalLabels = names
-            .map(label => label.toLowerCase())
             .filter(label => TOO_GENERIC_LABELS.indexOf(label) === -1)
             .filter(label => label.indexOf('life') == -1)
+        console.log('Filtered labels: ' + animalLabels);
             
         const emoji = animalLabels.map(label => EMOJIS[label] || '')
             .filter(emoji => emoji !== '')
             .join('');
         console.log('Found emoji: ' + emoji);
         
-        const additionalLabel = animalLabels.length > 0 ? ('/' + animalLabels[1]) : '';
+        const additionalLabel = animalLabels.length > 1 ? ('/' + animalLabels[1]) : '';
         
         const msg = isAnimal
             ? `I know! It is a ${animalLabels[0]}${additionalLabel}, am I right? ${emoji}${emoji}`
